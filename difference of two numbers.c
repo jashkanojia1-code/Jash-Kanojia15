d9e3912302dae02e04727d7a57c869df18152313
jashkanojia1-code <jashkanojia1@gmail.com>
2025-08-23 20:46:27 +0530

    Add files via upload

---
+++ difference of two numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,diff;
+    printf("Enter first number:   \n");
+    scanf("%d", &a);
+    printf("Enter second number:  \n");
+    scanf("%d", &b);
+
+    diff=a-b;
+    printf("The difference is: %d", diff);
+    return 0;
+}
