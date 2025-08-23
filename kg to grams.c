8c819eefa35ea4ac5455d29759028b10092d62e0
jashkanojia1-code <jashkanojia1@gmail.com>
2025-08-24 00:53:19 +0530

    Add files via upload

---
+++ kg to grams.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float kg;
+    printf("Enter kg: \n");
+    scanf("%f", &kg);
+
+
+    printf("The given value in grams is :  %f", kg*1000);
+
+    return 0;
+}
