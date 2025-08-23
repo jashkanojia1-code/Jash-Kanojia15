39d6183a6c971dfd3168aabb275e1e91c629e4d2
jashkanojia1-code <jashkanojia1@gmail.com>
2025-08-23 20:55:34 +0530

    Add files via upload

---
+++ division of two.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float a,b,division;
+    printf("Enter first number:   \n");
+    scanf("%f", &a);
+    printf("Enter second number:  \n");
+    scanf("%f", &b);
+
+    division=a/b;
+    printf("The division is: %f", division);
+    return 0;
+}
