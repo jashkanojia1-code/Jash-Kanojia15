fc147ac585936955231ae0123789c1d6bf75422e
jashkanojia1-code <jashkanojia1@gmail.com>
2025-08-25 00:37:26 +0530

    Add files via upload

---
+++ total,average,grades if else.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+int main()
+{
+
+    float m1,m2,m3,average;
+    printf("Enter marks for first subject:  \n");
+    scanf("%f",&m1);
+    printf("Enter marks for second subject:  \n");
+    scanf("%f",&m2);
+    printf("Enter marks for third subject:  \n");
+    scanf("%f",&m3);
+
+    average=(m1+m2+m3)/3;
+    printf("The total marks are: %f \n",m1+m2+m3);
+    printf("The average marks are: %f \n",average);
+
+    if(m1<35 || m2<35 || m3<35)
+    {
+        printf("Fail\n");
+        return 0;
+    }
+
+    if(average>=70)
+    {
+        printf("Distinction \n");
+    }
+    else if(average>=60)
+            {
+                printf("First \n");
+            }
+                else if(average>=50)
+                {
+                    printf("Second \n");
+                }
+                else if(average>=35)
+                {
+                    printf("Third class");
+                }
+                else
+                {
+                   printf("Fail\n");
+                }
+
+
+
+
+
+
+    return 0;
+}
