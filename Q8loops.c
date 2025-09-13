f9e43a18631b6c1474abcbd87dbdd032c331ec01
jashkanojia1-code <jashkanojia1@gmail.com>
2025-09-13 20:37:17 +0530

    Add files via upload

---
+++ Q8loops.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int i,n;
+    printf("Enter the number of odd numbers whose sum has to be calculated:\n");
+    scanf("%d",&n);
+
+
+    printf("The sum of first %d odd numbers is : \n",n);
+    int sum=0;
+    
+    for(i=1;i<=2*n-1;i+=2)
+    {
+        sum+=i;
+        
+    }
+    printf("%d", sum);
+    return 0;
+}
