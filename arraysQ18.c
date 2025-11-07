7bbbed8a3f81ea7474e9217b5a0b6454ace5f715
jashkanojia1-code <jashkanojia1@gmail.com>
2025-11-07 20:10:42 +0530

    Add files via upload

---
+++ arraysQ18.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main()
+{
+    int a[100],n,position;
+
+    printf("Enter the size of the array: \n");
+    scanf("%d", &n);
+
+    //input elements
+
+    printf("Enter array elements\n");
+
+    for(int i = 0; i < n;i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter the position of the element you want to delete(1 to %d):\n",n);
+    scanf("%d", &position);
+
+    int index = position -1;
+
+    for(int i = index; i < n - 1; i++)
+    {
+        a[i] = a[i+1];
+    }
+    n--;
+
+    //resulting array
+
+    printf("After deleting the first element: \n");
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d  \t", a[i]);
+    }
+
+    return 0;
+
+
+}
