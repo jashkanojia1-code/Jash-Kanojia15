7e9dd4b6a0dc121a3aaa7ace1e83346d4306c3c5
jashkanojia1-code <jashkanojia1@gmail.com>
2025-11-07 19:58:59 +0530

    Add files via upload

---
+++ arraysQ16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int a[100],n;
+
+    printf("Enter the size of the array: \n");
+    scanf("%d", &n);
+
+    //input elements
+
+    printf("Enter array elements\n");
+
+    for(int i = 0; i < n;i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    n--;
+
+    //resulting array
+
+    printf("After deleting the first element: \n");
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d  \t", a[i]);
+    }
+
+    return 0;
+
+
+}
