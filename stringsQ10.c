3b8d043a8d354d93ec4168b03b6ba5b2501f1b20
jashkanojia1-code <jashkanojia1@gmail.com>
2025-11-08 12:34:21 +0530

    Add files via upload

---
+++ stringsQ10.c
@@ -0,0 +1,20 @@
+// print the characters of a string vertically
+
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+
+    printf("Enter the String:  \n");
+    gets(str);
+
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+        printf("%c \n", str[i]);
+
+    }
+    
+
+    return 0;
+}
