76ea4222797af3e766fdc6309fb0c49575794104
jashkanojia1-code <jashkanojia1@gmail.com>
2025-11-13 22:52:51 +0530

    Add files via upload

---
+++ Q22recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int countDigits(int n) {
+    if (n == 0)
+        return 0;
+    return 1 + countDigits(n / 10);
+}
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("Digits: %d", countDigits(n));
+    return 0;
+}
