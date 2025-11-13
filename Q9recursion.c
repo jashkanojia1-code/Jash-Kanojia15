c5bd65d360049c5b56a0b3610e22250bf63b5a94
jashkanojia1-code <jashkanojia1@gmail.com>
2025-11-13 16:56:59 +0530

    Add files via upload

---
+++ Q9recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int sumEven(int n) {
+    if (n == 0)
+        return 0;
+    return (2 * n) + sumEven(n - 1);
+}
+
+int main() {
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    printf("Sum = %d", sumEven(n));
+    return 0;
+}
