63474c4a8025c0f38c6eff0a8a31c125c3f432d9
jashkanojia1-code <jashkanojia1@gmail.com>
2025-11-13 16:56:18 +0530

    Add files via upload

---
+++ Q8recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int sumOdd(int n) {
+    if (n == 0)
+        return 0;
+    return (2 * n - 1) + sumOdd(n - 1);
+}
+
+int main() {
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    printf("Sum = %d", sumOdd(n));
+    return 0;
+}
