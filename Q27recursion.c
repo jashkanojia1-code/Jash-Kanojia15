ceacafd9a149986d9797f2088f9bff866832709c
jashkanojia1-code <jashkanojia1@gmail.com>
2025-11-13 23:52:12 +0530

    Add files via upload

---
+++ Q27recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void factors(int n, int i) {
+    if (i > n)
+        return;
+    if (n % i == 0)
+        printf("%d ", i);
+    factors(n, i + 1);
+}
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    factors(n, 1);
+    return 0;
+}
