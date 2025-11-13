ce1ae24a6b91ec23db68b47ec52ccf74c53ba1af
jashkanojia1-code <jashkanojia1@gmail.com>
2025-11-13 23:54:56 +0530

    Add files via upload

---
+++ Q28recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int sumFactors(int n, int i) {
+    if (i > n / 2)
+        return 0;
+    if (n % i == 0)
+        return i + sumFactors(n, i + 1);
+    return sumFactors(n, i + 1);
+}
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (n == sumFactors(n, 1))
+        printf("Perfect number");
+    else
+        printf("Not perfect");
+    return 0;
+}
