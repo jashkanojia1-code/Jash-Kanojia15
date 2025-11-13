e6de1c13f0a89d11eb3aeb577c6c0cfe9b098a4c
jashkanojia1-code <jashkanojia1@gmail.com>
2025-11-13 16:55:14 +0530

    Add files via upload

---
+++ Q7recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int sumNatural(int n) {
+    if (n == 0)
+        return 0;
+    return n + sumNatural(n - 1);
+}
+
+int main() {
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    printf("Sum = %d", sumNatural(n));
+    return 0;
+}
