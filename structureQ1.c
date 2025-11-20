c8f7d59c788a7c3a9db62adcddd44a57e606bf14
jashkanojia1-code <jashkanojia1@gmail.com>
2025-11-20 20:25:16 +0530

    Refactor student structure and input/output functions

--- structureQ1.c
+++ structureQ1.c
@@ -1,71 +1,67 @@
 #include <stdio.h>
-#include <string.h>
 
-#define SIZE 10
-
-// Structure definition
 struct Student {
-    int roll_no;
-    char name[50];
-    char course[50];
-    char major[50];
-    char minor[50];
+    int roll;
+    char name[30];
+    char course[30];
+    char major[30];
+    char minor[30];
 };
 
-// Function to print all student names
-void printAllNames(struct Student s[], int n) {
-    printf("\nList of all students:\n");
-    for (int i = 0; i < n; i++) {
+// Function to print names of all students
+void printNames(struct Student s[]) {
+    printf("\nNames of all students:\n");
+    for(int i = 0; i < 10; i++) {
         printf("%s\n", s[i].name);
     }
 }
 
-// Function to print data of a student by roll number
-void printStudentByRoll(struct Student s[], int n, int roll) {
-    int found = 0;
-    for (int i = 0; i < n; i++) {
-        if (s[i].roll_no == roll) {
-            printf("\nDetails of student with Roll No %d:\n", roll);
-            printf("Name: %s\n", s[i].name);
-            printf("Course: %s\n", s[i].course);
-            printf("Major: %s\n", s[i].major);
-            printf("Minor: %s\n", s[i].minor);
+int main() {
+    struct Student s[10];
+    int i, r, found = 0;
+
+    // Input details
+    for(i = 0; i < 10; i++) {
+        printf("\nEnter details of student %d:\n", i+1);
+
+        printf("Roll number: \n");
+        scanf("%d", &s[i].roll);
+
+        printf("Name: \n");
+        scanf("%s", s[i].name);
+
+        printf("Course: \n");
+        scanf("%s", s[i].course);
+
+        printf("Major: \n");
+        scanf("%s", s[i].major);
+
+        printf("Minor: \n");
+        scanf("%s", s[i].minor);
+    }
+
+    // Call the function to print names
+    printNames(s);
+
+    // Search by roll number
+    printf("\nEnter roll number to search: ");
+    scanf("%d", &r);
+
+    for(i = 0; i < 10; i++) {
+        if(s[i].roll == r) {
+            printf("\nStudent Found:\n");
+            printf("Roll : %d\n", s[i].roll);
+            printf("Name : %s\n", s[i].name);
+            printf("Course : %s\n", s[i].course);
+            printf("Major  : %s\n", s[i].major);
+            printf("Minor  : %s\n", s[i].minor);
             found = 1;
             break;
         }
     }
-    if (!found) {
-        printf("\nNo student found with Roll No %d\n", roll);
-    }
-}
 
-int main() {
-    struct Student s[SIZE];
-    int roll;
-
-    printf("Enter details of %d students:\n", SIZE);
-    for (int i = 0; i < SIZE; i++) {
-        printf("\nStudent %d\n", i + 1);
-        printf("Enter Roll No: ");
-        scanf("%d", &s[i].roll_no);
-        getchar();  // clear newline
-        printf("Enter Name: ");
-        gets(s[i].name);
-        printf("Enter Course Name: ");
-        gets(s[i].course);
-        printf("Enter Major Subject: ");
-        gets(s[i].major);
-        printf("Enter Minor Subject: ");
-        gets(s[i].minor);
-    }
-
-    // (a) Print all student names
-    printAllNames(s, SIZE);
-
-    // (b) Print data of a student whose roll number is given
-    printf("\nEnter Roll No to search: ");
-    scanf("%d", &roll);
-    printStudentByRoll(s, SIZE, roll);
+    if(!found)
+        printf("Student not found.\n");
 
     return 0;
 }
